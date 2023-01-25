18277d351456d484a12fdc5a5aec11a4c89f08cd
leandregib <116063042+leandregib@users.noreply.github.com>
2023-01-25 16:27:26 +0100

    Projet

---
+++ galerie.php
@@ -0,0 +1,31 @@
+<?php
+	$strTitle 	= "PetSitter - Galerie photo";
+	include("header.php");
+    ?>
+
+    <main>
+        <div class="gallery-container">
+            <div class="gallery-item" data-index="1">
+                <img src="assets/img/imageGalerie1.jpg" alt="Photo de chat">
+            </div>
+            <div class="gallery-item" data-index="2">
+                <img src="assets/img/imageGalerie2.jpg" alt="Photo de chat">
+            </div>
+            <div class="gallery-item" data-index="3">
+                <img src="assets/img/imageGalerie3.jpg" alt="Photo de chat">
+            </div>
+            <div class="gallery-item" data-index="4">
+                <img src="assets/img/imageGalerie4.jpg" alt="Photo de chat">
+            </div>
+            <div class="gallery-item" data-index="5">
+                <img src="assets/img/imageGalerie5.jpg" alt="Photo de chat">
+            </div>
+            <div class="gallery-item" data-index="6">
+                <img src="assets/img/imageGalerie6.jpg" alt="Photo de chat">
+            </div>
+        </div>
+    </main>
+        
+        <?php
+	include("footer.php");
+    ?>
