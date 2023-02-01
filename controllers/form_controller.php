d252aecf1074be66b1b95f8007f76467b2c5dca0
TimKRN <104012948+TimKRN@users.noreply.github.com>
2023-02-01 13:32:59 +0100

    ajout controller form

---
+++ controllers/form_controller.php
@@ -0,0 +1,59 @@
+<?php
+	/**
+	* Controller des pages des formulaires
+	* @autor Timothée KERN
+	*/
+	class Form_controller extends Base_controller{
+		
+		public function petTypeForm(){
+			include("connect.php");
+			$strTitle 	= "#";
+			$strPage	= "petTypeForm";
+			include("views/header.php");
+			
+			// Récupérer les informations du formulaire
+			$strPetName 		= $_POST['petName']??'';
+			$strPetBirth		= $_POST['petBirth']??'';
+			$strSitterType		= $_POST['sitterType']??'';
+			$strPetSex			= $_POST['petSex']??'';
+			//var_dump($_POST);
+			//var_dump($_SERVER);
+			$arrErrors 	= array(); // Initialisation du tableau des erreurs
+			if (count($_POST) > 0){ // si formulaire envoyé
+				// Tests erreurs
+				if ($strPetName == ''){
+					$arrErrors['name'] = "Merci de renseigner un nom pour votre animal";
+				}
+				if ($strPetBirth == ''){
+					$arrErrors['birth'] = "Merci de renseigner une date de naissance";
+				}
+				if ($strSitterType == ''){
+					$arrErrors['type'] = "Merci de renseigner un type d'animal";
+				}
+				if ($strPetSex == ''){
+					$arrErrors['sex'] = "Merci de renseigner le sexe de votre animal";
+				}
+				
+				if (count($arrErrors)>0){ // Affichage des erreurs, s'il y en a
+					echo "<div class='error'>";
+					foreach($arrErrors as $strError){
+						echo "<p>".$strError."</p>";
+					}
+					echo "</div>";
+				}else{	
+						// Insertion en BDD, si pas d'erreurs
+						$strRqAdd 	= "	INSERT INTO articles 
+											(article_title, article_img, article_content, article_createdate, article_creator)
+										VALUES 
+											('".addslashes($strArticleTitle)."', '".$strNewName."', '".addslashes($strArticleContent)."', NOW(), 3);";
+						$db->exec($strRqAdd);
+						header("Location:index.php"); // Redirection page d'accueil
+					}
+				
+			}
+			include("views/footer.php");
+		}
+				
+		
+		
+	}
